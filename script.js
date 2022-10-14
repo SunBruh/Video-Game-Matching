aef5710a2a8aab613fea9b843b316ffb3acdfcf5
Jacob Beffrey <jacob.beffrey@Gmail.com>
2022-10-14 09:10:34 -0400

    updated a little bit of the css code and made an assets folder

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+const cardArray = [];
+const gameContainer = document.querySelector(".game-container");
+
+for (let i = 0; i <= 11; i++) {
+  let newDiv = document.createElement("div");
+  gameContainer.append(newDiv);
+  newDiv.classList.add("card");
+}
